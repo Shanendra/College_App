1b34994c1edfa27d486d1521c2f3d635a3cea45e
Shanendra <singhshanu204@gmail.com>
2024-02-08 01:24:11 +0530

    final

---
+++ app/src/main/java/com/example/vindhyagroupofinstitutions/activity_intro.java
@@ -0,0 +1,14 @@
+package com.example.vindhyagroupofinstitutions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_intro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
