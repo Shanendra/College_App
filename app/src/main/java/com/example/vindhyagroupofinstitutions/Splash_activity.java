ebc02423b944db0229960f4c46368299ad82d45a
Dheerendra22 <dheerendramishra64646@gmail.com>
2024-02-08 23:09:37 +0530

    splash

---
+++ app/src/main/java/com/example/vindhyagroupofinstitutions/Splash_activity.java
@@ -0,0 +1,28 @@
+package com.example.vindhyagroupofinstitutions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Splash_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.splash_activity);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(getApplicationContext(), Login_Activity.class));
+            finish();
+        },1500);
+    }
+}
